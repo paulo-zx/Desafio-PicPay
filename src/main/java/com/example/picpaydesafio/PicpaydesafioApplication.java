698cd094243353e225b4886551445141edbaf04d
paulo-zx <paulo-zx@hotmail.com>
2023-09-06 20:58:31 -0300

    feat: initial config

---
+++ src/main/java/com/example/picpaydesafio/PicpaydesafioApplication.java
@@ -0,0 +1,13 @@
+package com.example.picpaydesafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicpaydesafioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicpaydesafioApplication.class, args);
+	}
+
+}
